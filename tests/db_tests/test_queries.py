c4deed741a82c56cef75ddf2193905307fd286d3
Skrillite <nazarov.k.k@gmail.com>
2021-07-06 15:22:30 +0300

    new_account test

--- tests/db_tests/test_queries.py
+++ tests/db_tests/test_queries.py
@@ -8,17 +8,26 @@ from api.requests import BaseRequestData
 from configs import ApplicationConfigs
 
 
+admins_ids = [1, 2, 3]
+members_ids = [
+    (1, 11), (1, 12), (1, 13),
+    (2, 21), (2, 22), (2, 23),
+    (3, 31), (3, 32), (3, 33)
+]
+
+
 @pytest.fixture()
 async def prepare_db():
     session: AsyncSession = pytest.ctx.db.get().make_session()
 
+    yield
     async with session.begin():
         await session.execute('TRUNCATE members, payment_methods RESTART IDENTITY')
 
 
 class TestQueries:
     @pytest.mark.asyncio
-    async def test_connection(self):
+    async def test_connection(self, prepare_db):
         session: AsyncSession = pytest.ctx.db.get().make_session()
 
         async with session.begin():
@@ -26,10 +35,29 @@ class TestQueries:
                 'SELECT current_database()'
             )).fetchone()
 
-        await session.close()
-
         assert test == ApplicationConfigs.db.test_db_name
 
     @pytest.mark.asyncio
-    async def test_new_account(self):
+    @pytest.mark.parametrize('id', admins_ids)
+    async def test_new_account(self, id: int):
         session = pytest.ctx.db.get().make_session()
+
+        async with session.begin():
+            before = (await session.execute(
+                select(DBMembers.user_id)
+            )).all()
+
+            await queries.create_account(
+                session,
+                BaseRequestData.parse_obj({
+                    'requesting_user_id': id
+                }))
+
+            after = (await session.execute(
+                select(DBMembers.user_id)
+            )).all()
+
+        assert set(before + [(id, )]) == set(after)
+
+
+# TODO переписать транзакции в ручки
