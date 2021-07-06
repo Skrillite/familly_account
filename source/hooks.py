0c858c2b053905f19021b306e230e1a2740ea759
Skrillite <nazarov.k.k@gmail.com>
2021-07-06 14:38:50 +0300

    fix connection pool problem

--- source/hooks.py
+++ source/hooks.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.pool import NullPool, QueuePool
 from contextvars import ContextVar
 
 from configs import ApplicationConfigs
@@ -7,12 +8,16 @@ from db.database import DataBase
 
 def init_db_posgresql(database_context: ContextVar, test_db=False):
     url = ApplicationConfigs.db.url
+    pool_class = QueuePool
+
     if test_db:
         url = ApplicationConfigs.db.test_db_url
+        pool_class = NullPool
 
     engine = create_async_engine(
         url,
         pool_pre_ping=True,
+        poolclass=pool_class
     )
 
     database = DataBase(engine)
