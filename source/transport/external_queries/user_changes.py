24354bfd9a9b5ab08f80d8b8132dc65dbf761382
Skrillite <nazarov.k.k@gmail.com>
2021-07-13 17:34:30 +0300

    delete query fix

--- source/transport/external_queries/user_changes.py
+++ source/transport/external_queries/user_changes.py
@@ -3,29 +3,28 @@ from aiohttp import ClientSession
 from configs import ApplicationConfigs
 
 
-async def add_payment_method(user_ids: list[int], payment_methods: list[int]):
-    async with ClientSession() as session:
-        for id in user_ids:
-            async with session.post(
-                    url=ApplicationConfigs.ext.user_data_service_url,
-                    json={
-                        'id': id,
-                        'payment_methods': payment_methods
-                    }
-            ) as resp:
-                if resp.status != 200:
-                    raise Exception
+async def add_payment_method(user_ids: list[int], payment_methods: list[int], session: ClientSession):
+    for id in user_ids:
+        async with session.post(
+                ApplicationConfigs.ext.user_data_service_url,
+                json={
+                    'id': id,
+                    'payment_methods': payment_methods
+                }
+        ) as resp:
+            if resp.status != 200:
+                raise Exception
 
 
-async def delete_payment_method(user_ids: list[int], payment_methods: list[int]):
-    async with ClientSession() as session:
+async def delete_payment_method(user_ids: list[int], payment_methods: list[int], session: ClientSession):
+    async with session as session:
         for id in user_ids:
             async with session.delete(
-                url=ApplicationConfigs.ext.user_data_service_url,
+                ApplicationConfigs.ext.user_data_service_url,
                 json={
                     'id': id,
                     'payment_methods': payment_methods
                 }
             ) as resp:
                 if resp.status != 200:
-                    raise Exception('tmp')
+                    raise Exception
