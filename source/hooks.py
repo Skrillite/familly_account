1ac152b14cbf92c66d31eefae75523ac96447da0
Skrillite <nazarov.k.k@gmail.com>
2021-07-21 17:56:32 +0300

    external di modules

--- source/hooks.py
+++ source/hooks.py
@@ -26,25 +26,28 @@ def init_db_posgresql(database_context: ContextVar, test_db=False):
     database_context.set(database)
 
 
-def init_di(di: ContextVar):
-    dbq = DBQueryDI()
+def init_di(di: ContextVar, db_queries_di=None, ext_queries_di=None):
 
-    dbq.create_account = queries.create_account
-    dbq.delete_account = queries.delete_account
+    if not db_queries_di:
+        db_queries_di = DBQueryDI()
 
-    dbq.create_member = queries.create_member
-    dbq.delete_member = queries.delete_member
-    dbq.account_id_by_member = queries.account_id_by_member
-    dbq.members_by_account_id = queries.members_by_account_id
+        db_queries_di.create_account = queries.create_account
+        db_queries_di.delete_account = queries.delete_account
 
-    dbq.get_payment_method_by_account_id = queries.get_payment_methods
-    dbq.add_payment_method = queries.add_payment_method
-    dbq.delete_payment_method = queries.delete_payment_method
+        db_queries_di.create_member = queries.create_member
+        db_queries_di.delete_member = queries.delete_member
+        db_queries_di.account_id_by_member = queries.account_id_by_member
+        db_queries_di.members_by_account_id = queries.members_by_account_id
 
-    extq = ExtQueriesDI()
-    extq.add_payment_methods = external_queries.add_payment_method
-    extq.delete_payment_methods = external_queries.delete_payment_method
+        db_queries_di.get_payment_method_by_account_id = queries.get_payment_methods
+        db_queries_di.add_payment_method = queries.add_payment_method
+        db_queries_di.delete_payment_method = queries.delete_payment_method
+
+    if not ext_queries_di:
+        ext_queries_di = ExtQueriesDI()
+        ext_queries_di.add_payment_methods = external_queries.add_payment_method
+        ext_queries_di.delete_payment_methods = external_queries.delete_payment_method
 
     di.set(DI())
-    di.get().db_queries = dbq
-    di.get().external_queries = extq
+    di.get().db_queries = db_queries_di
+    di.get().external_queries = ext_queries_di
